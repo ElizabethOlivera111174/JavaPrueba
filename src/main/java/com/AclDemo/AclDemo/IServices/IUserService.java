25dec8334bb4604bd12799d21805e92804c6f45d
Luu Olivera <eliolivera570@gmail.com>
2021-11-26 00:09:57 -0300

    prueba

--- src/main/java/com/AclDemo/AclDemo/IServices/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.AclDemo.AclDemo.IServices;
-
-import com.AclDemo.AclDemo.Models.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserService extends JpaRepository<Users, Integer> {
-    Users findByName(String name);
-}
--- src/main/resources/import.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO USERS (id, name, email , password ) VALUES (1, 'user', 'user@gmail.com', '$2a$12$ZPVqBJwpGzhGOshBUOEbSetZu/7gm3R5Nj6VpvGlRkxqzMAXfm.9a' );
