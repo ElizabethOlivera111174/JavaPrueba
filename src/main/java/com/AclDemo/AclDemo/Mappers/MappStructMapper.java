f213c8de11ade5cde1a10adb4e575e82dc0cd64a
Luu Olivera <eliolivera570@gmail.com>
2021-11-29 06:46:14 -0300

    utimos cambios

--- src/main/java/com/AclDemo/AclDemo/Mappers/MappStructMapper.java
+++ src/main/java/com/AclDemo/AclDemo/Mappers/MappStructMapper.java
@@ -3,18 +3,20 @@ package com.AclDemo.AclDemo.Mappers;
 
 import com.AclDemo.AclDemo.Mappers.DTOs.CharactersDTOs.CharacterDTO;
 import com.AclDemo.AclDemo.Mappers.DTOs.CharactersDTOs.CharacterInUpDTO;
+import com.AclDemo.AclDemo.Mappers.DTOs.CharactersDTOs.CharacterInsertDTO;
 import com.AclDemo.AclDemo.Mappers.DTOs.MoviesDTOs.MoviesDTO;
 import com.AclDemo.AclDemo.Models.Character;
 import com.AclDemo.AclDemo.Models.Movie;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface  MappStructMapper {
 
-    CharacterDTO characterToCharacterDto(Character characterModel);
+    Character characterToCharacterDto(Character characterModel);
     Character CharacterInsetDtoToCharacter(CharacterInUpDTO characterInUpDTO);
+    Character CharacterInsertDtoToCharacter(CharacterInsertDTO characterInUpDTO);
     List<CharacterDTO> charactersToCharacterDTO(List<Character> characters);
 
     List<MoviesDTO> moviesToMovieDtos(List<Movie> movies);
@@ -22,4 +24,14 @@ public interface  MappStructMapper {
     MoviesDTO moviesToMovieDto(Movie movie);
 
     Movie moviesDTOToMovie(MoviesDTO movie);
+
+
+
+
+
+
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+            nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+    Character updateCharacterFromDto(CharacterInUpDTO characterDto, @MappingTarget Character character);
 }
